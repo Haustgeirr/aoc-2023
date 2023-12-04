f4d9525d579d579f9a016b2020473ae50dc0e40e
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-12-04 13:42:14 +0000

    4p2

--- 4/4.py
+++ 4/4.py
@@ -6,23 +6,51 @@ input = file.readlines()
 def get_numbers(number_set):
  return re.findall(r'\d{1,2}', number_set)
 
-def part_one(input):
+
+def get_matches(row):
+  row = row.rstrip()
+  [_, numbers] = row.split(': ')
+  [my_numbers, winning_numbers] = numbers.split('|')
+  my_numbers = get_numbers(my_numbers)
+  winning_numbers = get_numbers(winning_numbers)
+
+  def find_matches(target):
+    return target in winning_numbers
+
+  return len(list(filter(find_matches, my_numbers)))
+
+
+def get_winning_cards(index):
+  matches = get_matches(input[index])
+  winning_cards = []
+  
+  i = 1
+  while i <= matches:
+    winning_cards.append(get_winning_cards(index + i))
+    i += 1
+
+  return winning_cards
+
+
+def flatten(array):
+    if array == []:
+        return 1
+    if isinstance(array[0], list):
+        return flatten(array[0]) + flatten(array[1:])
+    return array[:1] + flatten(array[1:])
+
+
+def solve(input):
   score = 0
+  cards = []
 
-  for row in input:
-    row = row.rstrip()
-    [_, numbers] = row.split(': ')
-    [my_numbers, winning_numbers] = numbers.split('|')
-    my_numbers = get_numbers(my_numbers)
-    winning_numbers = get_numbers(winning_numbers)
+  for index, row in enumerate(input):
+    matches = get_matches(row)
+    score += 2 ** (matches - 1) if matches > 0 else 0
 
-    def find_matches(target):
-      return target in winning_numbers
+    cards.append(get_winning_cards(index))
 
-    matches = list(filter(find_matches, my_numbers))
-    score += 2 ** (len(matches) - 1) if len(matches) > 0 else 0
-
-  return score
+  return score, flatten(cards) - 1
 
 
-print(part_one(input))
+print(solve(input))
