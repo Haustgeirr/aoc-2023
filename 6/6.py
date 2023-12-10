759cbbb54a5d0c6187fce5c4cca1788496457103
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-12-10 21:46:41 +0000

    day 6 part 2

--- 6/6.py
+++ 6/6.py
@@ -25,4 +25,17 @@ def part_one(input):
     return math.prod(ways_to_win)
 
 
+def part_two(input):
+    time = int("".join(re.findall(r"\d+", input[0][9:])))
+    best = int("".join(re.findall(r"\d+", input[1][9:])))
+
+    way = 0
+    for i in range(0, time):
+        if (time - i) * i > best:
+            way += 1
+
+    return way
+
+
 print(part_one(input))
+print(part_two(input))
