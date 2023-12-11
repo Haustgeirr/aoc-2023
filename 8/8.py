569a64267d2c47f19cbcad45a85bd62759858382
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-12-11 19:22:09 +0000

    8.2 attempt 1

--- 8/8.py
+++ 8/8.py
@@ -1,15 +1,22 @@
 import re
+import math
 
 file = open("8/input.txt", "r")
 input = file.readlines()
 
 
-def part_one(input):
+def parse(input):
     route = input[0].strip()
     nodes = {
         key: (left, right)
         for [key, left, right] in (re.findall(r"\w{3}", row) for row in input[2:])
     }
+
+    return [route, nodes]
+
+
+def part_one(input):
+    [route, nodes] = parse(input)
 
     key = "AAA"
     steps = 0
@@ -21,4 +28,28 @@ def part_one(input):
     return steps
 
 
+def part_two(input):
+    [route, nodes] = parse(input)
+    starting_keys = [key for key in nodes.keys() if re.match(r".{2}A", key)]
+
+    keys = starting_keys.copy()
+    steps = 0
+    factors = []
+    while len(keys) > 0:
+        dir = route[steps % len(route)]
+        steps += 1
+
+        for i, key in enumerate(keys):
+            new_key = nodes[key][0] if dir == "L" else nodes[key][1]
+
+            if re.match(r".{2}Z", new_key):
+                keys.pop(i)
+                factors.append(steps)
+            else:
+                keys[i] = new_key
+
+    return math.lcm(*factors)
+
+
 print("p1: ", part_one(input))
+print("p2: ", part_two(input))
