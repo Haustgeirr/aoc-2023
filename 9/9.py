9b160990eb1b736f2dbaa0c59c7fb21743f19427
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-12-12 20:18:12 +0000

    9.2

--- 9/9.py
+++ 9/9.py
@@ -9,7 +9,7 @@ def diff(numbers):
 
 
 def part_one(input):
-    total = 0
+    p1 = p2 = 0
 
     for row in input:
         numbers = [[int(i) for i in row.split()]]
@@ -18,14 +18,16 @@ def part_one(input):
             d = diff(numbers[-1])
             numbers.append(d)
 
-        val = 0
+        p1v = p2v = 0
         for i in range(len(numbers) - 1, 0, -1):
-            val = numbers[i][-1] + val
+            p1v = numbers[i][-1] + p1v
+            p2v = numbers[i][0] - p2v
 
-        total += numbers[0][-1] + val
+        p1 += numbers[0][-1] + p1v
+        p2 += numbers[0][0] - p2v
 
-    return total
+    return [p1, p2]
 
 
-print("p1: ", part_one(input))
-# print("p2: ", part_two(input))
+print("p1: ", part_one(input)[0])
+print("p2: ", part_one(input)[1])
